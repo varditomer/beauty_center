6f5fe624474b75514c7fab25437b20e8c2f9019e
Dror Handler <hdror507@gmail.com>
2023-08-01 11:12:56 +0300

    adding table

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,26 +1,44 @@
-export default function Table({ titles, data }) {
-  return (
-    <div className="custom-table">
-      {/* Table Header */}
-      <div className="row header">
-        {titles.map((title) => (
-          <div key={title} className="cell">
-            {title}
-          </div>
-        ))}
-      </div>
+import { useEffect, useState } from "react";
 
-      {/* Table Rows */}
-      {data.map((row, rowIndex) => (
-        <div key={rowIndex} className="row">
-          {row.map((cell, cellIndex) => (
-            <div key={cellIndex} className="cell" data-title={titles[cellIndex]}>
-              {cell}
+export default function Table({ titles, data }) {
+  
+  const [newData, setNewData] = useState(null)
+  
+  useEffect(() => {
+    const newData = data.map((obj) => {
+      delete obj.id
+      return Object.values(obj)
+    })
+    setNewData(newData)
+  }, []);
+
+  console.log('data2', newData);
+  
+  return (
+    <>
+      {newData &&
+        <div className="custom-table">
+          {/* Table Header */}
+          <div className="row header">
+            {titles.map((title) => (
+              <div key={title} className="cell">
+                {title}
+              </div>
+            ))}
+          </div>
+          {/* Table Rows */}
+          {newData.map((row, idx) => {
+            return <div key={idx} className="row">
+              {row.map((cell, idx) => {
+                return <div key={idx} className="cell" data-title={titles[idx]}>
+                  {cell}
+                </div>
+              })}
             </div>
-          ))}
+          })}
         </div>
-      ))}
-    </div>
-  );
-};
+      }
+    </>
+  )
+}
 
